02dddd149306fa7f3e083f1240052c30145160e1
Alec Wertheimer <alec@wintreetz.com>
2018-12-17 22:23:25 -0500

    updated params

--- src/ComicDetail.js
+++ src/ComicDetail.js
@@ -8,7 +8,7 @@ class ComicDetail extends Component {
 
   async componentDidMount() {
     try {
-      const res = await fetch(`https://gateway.marvel.com:443/v1/public/series?contains=trade%20paperback${this.props.match.params.id}$ts=$apikey=66ef26227cbc5462b13d6310153279e2$hash=`);
+      const res = await fetch(`https://gateway.marvel.com:443/v1/public/series/${this.props.match.params.id}?ts={hash.getExpiryTime(300);}&apikey=66ef26227cbc5462b13d6310153279e2&hash={hash.append(hash.getExpiryTime(240)).append("2e5d72da891f2b5ca57fe0b1a06a26f20a622220").append("66ef26227cbc5462b13d6310153279e2").encodeMd5().toHex().printDigest();}`);
       const comic = await res.json();
       this.setState({
         comic,
