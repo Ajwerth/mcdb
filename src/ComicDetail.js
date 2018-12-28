dabcfb9f09f9e5f60e6f51948e4af04e94c1bea0
Alec Wertheimer <alec@wintreetz.com>
2018-12-28 11:12:10 -0500

    Added Comments in code

--- src/ComicDetail.js
+++ src/ComicDetail.js
@@ -1,17 +1,18 @@
 /* eslint react/destructuring-assignment:0 */
 import React, { Component } from 'react';
 
+
 class ComicDetail extends Component {
   state = {
-    comic: {},
+    comics: [],
   }
 
   async componentDidMount() {
     try {
-      const res = await fetch(`https://gateway.marvel.com:443/v1/public/series/${this.props.match.params.id}apikey=66ef26227cbc5462b13d6310153279e2`);
-      const comic = await res.json();
+      const res = await fetch('https://gateway.marvel.com:443/v1/public/series?contains=trade%20paperback&apikey=66ef26227cbc5462b13d6310153279e2');
+      const comics = await res.json();
       this.setState({
-        comic,
+        comics: comics.data.results,
       });
     } catch (e) {
       console.log(e);
@@ -19,16 +20,11 @@ class ComicDetail extends Component {
   }
 
   render() {
-    const results = this.state.comic.data.results.map(result => (
-      <div key={result.id}>
-        <h1>{result.title}</h1>
-        <p>{result.startYear}</p>
-      </div>
-    ));
     return (
       <div>
-        {results}
+        <div>{this.state.comics}</div>
       </div>
+
     );
   }
 }
