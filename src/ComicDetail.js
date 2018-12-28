a9ea5535093facbe93dce8e9896aee238acc1a47
Alec Wertheimer <alec@wintreetz.com>
2018-12-28 11:18:44 -0500

    fixed merge conflicts in ComicList from old-state of app

--- src/ComicDetail.js
+++ src/ComicDetail.js
@@ -11,13 +11,8 @@ class ComicDetail extends Component {
   // async await for the API Fetch
   async componentDidMount() {
     try {
-<<<<<<< HEAD
-      const res = await fetch(`https://gateway.marvel.com:443/v1/public/series/${this.props.match.params.id}?ts={hash.getExpiryTime(300);}&apikey=66ef26227cbc5462b13d6310153279e2&hash={hash.append(hash.getExpiryTime(240)).append("2e5d72da891f2b5ca57fe0b1a06a26f20a622220").append("66ef26227cbc5462b13d6310153279e2").encodeMd5().toHex().printDigest();}`);
-      const comic = await res.json();
-=======
       const res = await fetch('https://gateway.marvel.com:443/v1/public/series?contains=trade%20paperback&apikey=66ef26227cbc5462b13d6310153279e2');
       const comics = await res.json();
->>>>>>> old-state
       this.setState({
         comics: comics.data.results,
       });
@@ -30,13 +25,8 @@ class ComicDetail extends Component {
   render() {
     return (
       <div>
-<<<<<<< HEAD
-        <h1>Hello</h1>
-=======
         <div>{this.state.comics}</div>
->>>>>>> old-state
       </div>
-
     );
   }
 }
