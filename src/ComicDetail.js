39ef0e02fbe064a4c09845502f50042486d8f020
Alec Wertheimer <alec@wintreetz.com>
2018-12-28 11:12:36 -0500

    Added Comments in code

--- src/ComicDetail.js
+++ src/ComicDetail.js
@@ -1,12 +1,14 @@
 /* eslint react/destructuring-assignment:0 */
+// Import Dependencies
 import React, { Component } from 'react';
 
-
+// Create ComicDetail Componenet for Individual Comic Details
 class ComicDetail extends Component {
   state = {
     comics: [],
   }
 
+  // async await for the API Fetch
   async componentDidMount() {
     try {
       const res = await fetch('https://gateway.marvel.com:443/v1/public/series?contains=trade%20paperback&apikey=66ef26227cbc5462b13d6310153279e2');
@@ -19,6 +21,7 @@ class ComicDetail extends Component {
     }
   }
 
+  // Render Comic
   render() {
     return (
       <div>
